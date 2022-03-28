36cfa28308df2130495d2a3364d6350add8b9f0f
jonatas <jonataseduardo@gmail.com>
2022-03-28 15:30:04 -0300

    final commit

--- shape-tech-test/jonatas-cesar-shape-test-tech.py
+++ shape-tech-test/jonatas-cesar-shape-test-tech.py
@@ -3,12 +3,11 @@
 # Shape Tech-Test
 
 Author: Jonatas Cesar 
-
 """
-
 
 # %%
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas
 import pandas as pd
 import shap
@@ -22,7 +21,13 @@ from sklearn.utils.class_weight import compute_sample_weight
 #%matplotlib inline
 
 # %%
+"""
+## 0 - Data profiling
+"""
+
+# %%
 df = pd.read_excel("O&G Equipment Data.xlsx", index_col="Cycle")
+df[["Preset_1", "Preset_2"]] = df[["Preset_1", "Preset_2"]].astype("category")
 
 # %%
 df.describe().T
@@ -39,7 +44,80 @@ profile.to_widgets()
 
 # %%
 """
-## Split Data
+## 1 - Calculating how many times the equiment has failed
+"""
+
+# %%
+pd.DataFrame(df.Fail.value_counts())
+
+# %%
+"""
+## 2 - Categorize equipment failures by setups configurations (preset 1 and preset 2)
+"""
+
+# %%
+
+
+def eval_presetfail(df, preset_col):
+    preset_fail = (
+        pd.DataFrame(df.loc[:, [preset_col, "Fail"]].value_counts(), columns=["Counts"])
+        .reset_index()
+        .pivot(index=preset_col, columns="Fail", values="Counts")
+    )
+
+    preset_fail.columns = ["Not Fail Count", "Fail Count"]
+
+    preset_fail["Fail rate"] = preset_fail.loc[:, "Fail Count"] / (
+        preset_fail.loc[:, "Not Fail Count"] + preset_fail.loc[:, "Fail Count"]
+    )
+    return preset_fail
+
+
+# %%
+eval_presetfail(df, "Preset_1")
+
+
+# %%
+eval_presetfail(df, "Preset_2")
+
+
+# %%
+"""
+## 3 – Categorize equipment failures by their nature/root cause according to parameter readings
+"""
+
+
+# %%
+parameter_readings = [
+    "Temperature",
+    "Pressure",
+    "VibrationX",
+    "VibrationY",
+    "VibrationZ",
+    "Frequency",
+]
+
+# %%
+sns.pairplot(df.loc[:, parameter_readings + ["Fail"]], hue="Fail")
+plt.show()
+
+# %%
+
+mean_readings = df.groupby("Fail").mean().T
+mean_readings.columns = ["Not Fail Avg", "Fail Avg"]
+mean_readings["Avg Increase (%)"] = 100 * (
+    mean_readings["Fail Avg"] / mean_readings["Not Fail Avg"] - 1
+)
+mean_readings
+
+# %%
+"""
+## 4 - Create a model using the technique you think is most appropriate and measure its performance 
+"""
+
+# %%
+"""
+## 4.1 - Split Data
 """
 
 # %%
@@ -50,9 +128,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+
 # %%
 """
-## Model Fit 
+### 4.2 - Model Fit 
 """
 
 # %%
@@ -71,7 +150,7 @@ model.fit(
 
 # %%
 """
-## Classification perfomance
+## 4.3 - Classification perfomance
 """
 
 # %%
@@ -89,7 +168,7 @@ print(classification_report(y_test.values, y_pred))
 
 # %%
 """
-## Feature importance 
+## 5 - Analyze variable importance
 """
 
 # %%
